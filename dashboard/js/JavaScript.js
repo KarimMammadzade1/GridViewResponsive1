de22ed215f5b1499b731ab3a4f4e07dbfbd187e3
KarimMammadzade1 <63442186+KarimMammadzade1@users.noreply.github.com>
2020-05-23 14:41:01 +0400

    Add files via upload

---
+++ dashboard/js/JavaScript.js
@@ -0,0 +1,74 @@
+ function rengDeyis() 
+ {
+ 	if(document.getElementById("myCheck").checked)
+ 	{
+ 		document.getElementById('rengdeyis').style.backgroundColor='white';
+ 		document.getElementById('myBar').style.backgroundColor='hsl(225, 100%, 98%)';
+ 		document.getElementById('myH1').style.color='hsl(232, 19%, 15%)';
+ 		document.getElementById('myP1').style.color='hsl(232, 19%, 15%)';
+ 		document.getElementById('myToggletext').style.color='hsl(232, 19%, 15%)';
+ 		document.getElementById('myCard1').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCard2').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCard3').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCard4').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCard4').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myTextNumber1').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myTextNumber2').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myTextNumber3').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myTextNumber4').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myUserName1').style.color='gray';
+ 		document.getElementById('myUserName2').style.color='gray';
+ 		document.getElementById('myUserName3').style.color='gray';
+ 		document.getElementById('myUserName4').style.color='gray';
+        document.getElementById('myTextNumbersml1').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myTextNumbersml2').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myTextNumbersml3').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myTextNumbersml4').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('myCardsml1').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCardsml2').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCardsml3').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('myCardsml4').style.backgroundColor='hsl(227, 47%, 96%)';
+ 		document.getElementById('sml1').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('sml2').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('sml3').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('sml4').style.color='hsl(230, 17%, 14%)';
+ 		document.getElementById('h1sml').style.color='gray';
+ 	}
+        
+ 	else
+ 	{
+ 		document.getElementById('rengdeyis').style.backgroundColor='hsl(230, 17%, 14%)';
+ 		document.getElementById('myBar').style.backgroundColor='hsl(232, 19%, 15%)';
+ 		document.getElementById('myH1').style.color='white';
+ 		document.getElementById('myP1').style.color='white';
+ 		document.getElementById('myToggletext').style.color='white';
+ 		document.getElementById('myCard1').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myCard2').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myCard3').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myCard4').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myTextNumber1').style.color='white';
+ 		document.getElementById('myTextNumber2').style.color='white';
+ 		document.getElementById('myTextNumber3').style.color='white';
+ 		document.getElementById('myTextNumber4').style.color='white';
+ 		document.getElementById('myUserName1').style.color='white';
+ 		document.getElementById('myUserName2').style.color='white';
+ 		document.getElementById('myUserName3').style.color='white';
+ 		document.getElementById('myUserName4').style.color='white';
+ 		document.getElementById('myTextNumbersml1').style.color='white';
+ 		document.getElementById('myTextNumbersml2').style.color='white';
+ 		document.getElementById('myTextNumbersml3').style.color='white';
+ 		document.getElementById('myTextNumbersml4').style.color='white';
+ 		document.getElementById('sml1').style.color='white';
+ 		document.getElementById('sml2').style.color='white';
+ 		document.getElementById('sml3').style.color='white';
+ 		document.getElementById('sml4').style.color='white';
+ 	    document.getElementById('myCardsml1').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myCardsml2').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myCardsml3').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('myCardsml4').style.backgroundColor='hsl(228, 28%, 20%)';
+ 		document.getElementById('h1sml').style.color='white';
+ 		
+
+ 	}
+
+ }
